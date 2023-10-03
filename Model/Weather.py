53112bdaa5f35ca7e2894a7478972ca09ce1b534
Gmzakros <gmzakros@millersville.edu>
2023-10-03 09:45:32 -0400

    making cli

--- Model/Weather.py
+++ Model/Weather.py
@@ -1,11 +1,18 @@
 import ParseData
+import datetime
 
 
 def hourlyData():
-    dict = ParseData.getData()
-    return dict['hourly']
+    dict = ParseData.getData()['hourly']
+    timesAndTemps = {}
+    for i in range(len(dict['time'])):
+        estTime = datetime.datetime.fromtimestamp(dict['time'][i]).strftime('%H')
+        timesAndTemps[estTime] = dict['temperature_2m'][i]
+    return timesAndTemps
 
 
 def getWindSpeed():
     dict = ParseData.getData()
     return dict
+
+print(hourlyData())
---
+++ View/ViewCLI.py
@@ -0,0 +1,20 @@
+
+
+def printStart():
+    print()
+    print('Welcome to Wild Weather')
+    print('Here is the forcast for today:\n')
+
+
+    #Place Holder Values
+def printTable():
+    str = '{0: ^15}{1: ^15}{2: ^15}{3: ^15}{4: ^15}'.format('Current Tempature', 'Low', 'High', 'Humidity', 'Wind Speed')
+    data = '  {0: ^15}{1: ^15}{2: ^15}{3: ^15}{4: ^15}'.format('60', '58', '80', '75%', '12 mph')
+    print(str)
+    print('------------------------------------------------------------------------------------')
+    print(data)
+
+def main():
+    printStart()
+    printTable()
+    
