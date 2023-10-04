87409dc62b40f432dd52fb28f1edc2a957ac0070
Gmzakros <121960412+Gmzakros@users.noreply.github.com>
2023-10-04 08:52:00 -0400

    CLI Almost complete

--- Src/Model/ParseData.py
+++ Src/Model/ParseData.py
@@ -1,8 +1,13 @@
 import requests
 
 
-def getData():
+def getData1Day():
     response = requests.get("https://api.open-meteo.com/v1/forecast?latitude=39.9979&longitude=-76.3541&hourly=temperature_2m,relativehumidity_2m,rain,showers,visibility,windspeed_10m&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime&timezone=America%2FNew_York&forecast_days=1")
+    dict = response.json()
+    return dict
+
+def getData7Days():
+    response = requests.get("https://api.open-meteo.com/v1/forecast?latitude=39.9979&longitude=-76.3541&hourly=temperature_2m,precipitation_probability&daily=temperature_2m_max,temperature_2m_min&temperature_unit=fahrenheit&timeformat=unixtime&timezone=America%2FNew_York&past_days=1")
     dict = response.json()
     return dict
 
